e163892c5f46ac6a67d3c4b90272d225722bfff3
Luka <luka.luka.luka93@gmail.com>
2024-06-19 16:48:01 -0700

    pulling up quotes with api

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 const famousButton = document.querySelector("#famous-quotes");
 const inspButton = document.querySelector("#inspirational");
 const h1 = document.querySelector("h1.the-quote");
+
+const getData = async function () {
+    const getQuotes = await fetch("https://api.quotable.io/random");
+    const data = await getQuotes.json();
+    console.log(data)
+
+}
+getData();
