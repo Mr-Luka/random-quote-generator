7f6ded2ed6d559e25ff63c1739e3ba2641bf346e
Luka <luka.luka.luka93@gmail.com>
2024-06-19 17:05:52 -0700

    async data

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 const famousButton = document.querySelector("#famous-quotes");
 const inspButton = document.querySelector("#inspirational");
 const h1 = document.querySelector("h1.the-quote");
+const author = document.querySelector("h2")
 
 const getData = async function () {
-    const getQuotes = await fetch("https://api.quotable.io/random");
-    const data = await getQuotes.json();
-    console.log(data)
-
+    try {
+        const getQuotes = await fetch("https://api.quotable.io/random");
+        const data = await getQuotes.json();
+        return {quote: data.content, author:data.author}
+    }   catch (error) {
+        console.error("Error fetching the quote", error);
+    }
 }
 getData();
